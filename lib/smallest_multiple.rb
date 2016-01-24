43b7b1e70695de860f168200179f702b9b6cc75f
dcarrano <dcarrano@gmail.com>
2016-01-24 01:07:29 +0000

    Done.

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,22 @@
-# Implement your procedural solution here!
+def smallest_multiple(input)
+
+  i = 3
+
+  if (input % 2) != 0 then current_number = (input + 1) else current_number = input end
+
+  until i > input
+
+    until (current_number % i) != 0 || i > input
+      i += 1
+    end
+
+    if i <= input then
+      current_number += 2
+      i = 3
+    end
+
+  end
+
+  current_number
+
+end
